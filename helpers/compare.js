0ebeb82ec7fd435d14a9ff33e502c769cddf6b25
vaibhav soni <vssonivaibhav6@gmail.com>
2020-01-18 18:44:10 +0530

    cases aded

--- helpers/compare.js
+++ helpers/compare.js
@@ -1,8 +1,13 @@
+const priority = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1];
+
 const createMeta = (deck) => {
     return deck.reduce((result, card, index, deck) => {
         result.total += card.value;
+        if (card.value > result.highest) {
+            result.highest = card.value;
+        }
         result.values.push(card.value);
-        if (result.uniqueValues.indexOf(card.value === -1)) {
+        if (result.uniqueValues.indexOf(card.value) === -1) {
             result.uniqueValues.push(card.value)
         } else {
             result.pair = true;
@@ -16,25 +21,43 @@ const createMeta = (deck) => {
         if (index === deck.length - 1) {
             if (result.uniqueValues.length === 1) {
                 trail = true;
-                trailvalue =  result.uniqueValues[0];
+                trailvalue = result.uniqueValues[0];
+            }
+            if (result.uniqueValues.length === 3) {
+                result.uniqueValues.sort((a, b) => a - b)
+                if (result.uniqueValues[2] - result.uniqueValues[0] === 2) {
+                    result.sequence = true;
+                }
             }
         }
         return result;
     }, {
         total: 0,
+        highest: 0,
         values: [],
         uniqueValues: [],
         trail: false,
         trailvalue: 0,
         pair: false,
         pairValue: 0,
-        counts: {}
+        counts: {},
+        sequence: false
     })
 }
 
-
-const compare = (deck1, deck2) => {
-    deck1
+const compare = (p1, p2) => {
+    let result = {
+        winner: null,
+        rule: null
+    };
+    //Case for 2 trails
+    //case for 1 trail
+    //case for 1 sequence
+    //case for 2 sequence
+    //case for 1 pair
+    //case for 2 pair
+    //case for Top card
+    //cases for draw
 }
 
 export { createMeta, compare }
